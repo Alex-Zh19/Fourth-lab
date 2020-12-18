7e3a81bbecdce9c8e9c3a242903eef2751f9840b
Alexandr Zhalnerchik <alexzhalnerchikz@mail.ru>
2020-12-18 11:30:33 +0300

    frame

--- src/Frame.java
+++ src/Frame.java
@@ -1,2 +1,120 @@
-public class Frame {
-}
+import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.*;
+
+public class Frame extends JFrame {
+
+    private JFileChooser fileChooser=null;
+
+    private boolean fileLoaded=false;
+    private Display display = new Display();
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+
+    private JCheckBoxMenuItem showAxisMenuItem;
+    private JCheckBoxMenuItem showMarkersMenuItem;
+
+    public Frame(){
+        super("Построение графиков функций на основе подготовленных файлов");
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        setLocation((kit.getScreenSize().width - WIDTH)/2,
+                (kit.getScreenSize().height - HEIGHT)/2);
+        setExtendedState(MAXIMIZED_BOTH);
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        JMenu fileMenu = new JMenu("Файл");
+        menuBar.add(fileMenu);
+        Action openGraphicsAction = new AbstractAction("Открыть файл"){
+            public void actionPerformed(ActionEvent arg0) {
+                if (fileChooser==null) {
+                    fileChooser = new JFileChooser();
+                    fileChooser.setCurrentDirectory(new File("."));
+                }
+                if (fileChooser.showOpenDialog(Frame.this) == JFileChooser.APPROVE_OPTION);
+                openGraphics(fileChooser.getSelectedFile());
+            }
+        };
+        fileMenu.add(openGraphicsAction);
+        JMenu graphicsMenu = new JMenu("График");
+        menuBar.add(graphicsMenu);
+        Action showAxisAction = new AbstractAction("Показывать оси координат") {
+            public void actionPerformed(ActionEvent e) {
+                display.setShowAxis(showAxisMenuItem.isSelected());
+            }
+        };
+        showAxisMenuItem = new JCheckBoxMenuItem(showAxisAction);
+        graphicsMenu.add(showAxisMenuItem);
+        showAxisMenuItem.setSelected(true);
+
+
+        Action showMarkersAction = new AbstractAction("Показывать маркеры точек") {
+
+            public void actionPerformed(ActionEvent e) {
+                display.setShowMarkers(showMarkersMenuItem.isSelected());
+            }
+        };
+        showMarkersMenuItem = new JCheckBoxMenuItem(showMarkersAction);
+        graphicsMenu.add(showMarkersMenuItem);
+        showMarkersMenuItem.setSelected(true);
+        graphicsMenu.addMenuListener(new GraphicsMenuListener());
+        getContentPane().add(display, BorderLayout.CENTER);
+    }
+
+    protected void openGraphics(File selectedFile) {
+        try {
+            DataInputStream in = new DataInputStream(
+                    new FileInputStream(selectedFile));
+
+            Double[][] graphicsData = new
+                    Double[in.available()/(Double.SIZE/8)/2][];
+            int i = 0;
+            while (in.available() > 0) {
+                Double x = Double.valueOf(in.readDouble());
+                Double y = Double.valueOf(in.readDouble());
+                graphicsData[i++] = new Double[] {x, y};
+            }
+            if (graphicsData!=null && graphicsData.length>0) {
+                fileLoaded = true;
+                display.showGraphics(graphicsData);
+
+            }
+            in.close();
+        }catch (FileNotFoundException e){
+            JOptionPane.showMessageDialog(Frame.this,
+                    "Указанный файл не найден", "Ошибка загрузки данных",
+                    JOptionPane.WARNING_MESSAGE);
+            return;
+
+        }catch (IOException e){
+            JOptionPane.showMessageDialog(Frame.this,
+                    "Ошибка чтения координат точек из файла",
+                    "Ошибка загрузки данных", JOptionPane.WARNING_MESSAGE);
+            return;
+        }
+    }
+
+    private class GraphicsMenuListener implements MenuListener {
+        public void menuSelected(MenuEvent e) {
+            showAxisMenuItem.setEnabled(fileLoaded);
+            showMarkersMenuItem.setEnabled(fileLoaded);
+        }
+
+        public void menuDeselected(MenuEvent e) {
+        }
+
+        public void menuCanceled(MenuEvent e) {
+
+        }
+
+    }
+    public static void main(String[] args)
+    {
+        Frame frame = new Frame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+} 
